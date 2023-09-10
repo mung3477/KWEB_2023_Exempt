65ae074bdb05e5863c4b4570aa3481f38d0202fa
Woosung Joung <mung3477@naver.com>
2023-09-10 22:03:37 +0900

    feat: 강의 개설 및 조회

--- src/pages/api/courses/index.js
+++ src/pages/api/courses/index.js
@@ -2,7 +2,22 @@ import prisma from "lib/prisma";
 
 export default async function handle(req, res) {
 	if (req.method === "GET") {
-		const result = await prisma.course.findMany();
+		const { professorId } = req.query;
+		const result = await prisma.course.findMany({
+			where: {
+				professorId,
+			},
+		});
+		res.json(result);
+	}
+	if (req.method === "POST") {
+		const { name, professorId } = req.body;
+		const result = await prisma.course.create({
+			data: {
+				name,
+				professorId,
+			},
+		});
 		res.json(result);
 	}
 }
