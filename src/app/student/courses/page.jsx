97febfb25e3ed465313e3ad648f7ab9bf573e21f
Woosung Joung <mung3477@naver.com>
2023-09-10 16:52:50 +0900

    chore: delete console.log

--- src/app/student/courses/page.jsx
+++ src/app/student/courses/page.jsx
@@ -3,7 +3,7 @@ import { GETCourses } from "@/app/api/courses/route";
 export default async function CoursesPage() {
 	const res = await GETCourses();
 	const { courses } = await res.json();
-	console.log(courses);
+
 	return (
 		<ul>
 			{courses.map((course) => (
